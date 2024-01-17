b3d34c7b843ec3ff60d3c93f963c11c6f6923afd
mzaini30 <muhzaini30@gmail.com>
2024-01-18 06:51:33 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,6 @@
 <script>
   import Router from "svelte-spa-router";
-  import Beranda from "./Beranda.svelte";
-  import About from "./About.svelte";
-  const routes = {
-    "/": Beranda,
-    "/about": About,
-  };
+  const routes = {};
 </script>
 
 <Router {routes} />
